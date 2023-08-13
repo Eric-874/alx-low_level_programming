17c2f8194bc3b52a80c5aa69d251056c02005b0f
Eric-874 <eantwi874@gmail.com>
2023-08-13 18:00:44 -0400

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,15 @@
 
 
 /**
- * main - print " programming is like building a multilingual
- * puzzle, followed by a newline 
- * return 0.
- */ 
-
+ * main - print " programming is like building a multilingual					
+ * puzzle, followed by a newline								
+ * return 0.											
+ */ 											
 
 int main(void)
 
 
 {
-	puts("\" programming is like building a multilingual puzzule");
+	puts("\" programming is like building a multilingual puzzule");			
 	 return (0);
-} 	  
+}												
