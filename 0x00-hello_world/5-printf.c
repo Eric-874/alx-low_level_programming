0f2327474d8d4483e03de84b0d32e90e56a39cee
Eric-874 <eantwi874@gmail.com>
2023-08-14 08:45:09 -0400

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <srdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a place of art,\n");
+	return (0);
+}
