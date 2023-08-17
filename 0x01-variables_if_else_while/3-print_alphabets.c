2a749f57eff20f39b5ec50e4a999e184e1fdbe39
Eric-874 <eantwi874@gmail,com>
2023-08-17 11:28:46 -0400

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,10 +5,14 @@
  * using the putchar function.
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
+	
 	char lowercase = 'a';
+	
 	char uppercase = 'A';
+	
 	while (lowercase <= 'z')
 	{
 	putchar(lowercase);
@@ -22,7 +26,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 00ca0c5c3064e39014450bad2419c02e674fd345
