1680c8c642977a90e9b520c44397ab8b64321550
Eric-874 <eantwi874@gmail,com>
2023-08-21 19:14:44 -0400

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  * reset_to_98 - take a pointer to an int and update
  * the value it piont to to 98
  *
- * @n An integer input pointer
+ * @n An integer n  input pointer
  *
  * Return: nothing
  */
