b3b0ff695b01dd342f768e9fe92b8643c88c341d
Eric-874 <eantwi874@gmail,com>
2023-08-21 20:40:17 -0400

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _put -prints a string 
+ * @str: the string to printr
+ * Return: void
+ */
+void  _puts(char *str)
+{
+	for (; *str 1 = '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
